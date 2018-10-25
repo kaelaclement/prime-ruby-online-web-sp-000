116f56891cab63f5fcc2e3a7f3c45d2d96dce62f
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-25 20:56:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,5 @@
 def prime?(n)
-  if n > 0
-    divisors = (2..n - 1).to_a
-  else
-    divisors = (n + 1..-2)
-  end
+  n = n.abs
+  divisors = (2..n - 1).to_a
   !divisors.any? {|i| n % i == 0}
 end
